29f619fefdf9ff78f8ba3d7c442933046f22181c
Majm1k <ru.zaykin@mail.ru>
2021-12-29 17:56:27 +0300

    Initial camera setup

--- AI_ForTheCamera/ViewController.swift
+++ AI_ForTheCamera/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import AVKit //Аудио-визуальная библиотек для работы камеры
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Настройка камеры
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
     }
-
-
 }
 
