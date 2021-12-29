24644cd610215a0f8bdbd04d1a58d1b6a5aaf06a
Majm1k <ru.zaykin@mail.ru>
2021-12-29 17:30:41 +0300

    Initial Commit

---
+++ AI_ForTheCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI_ForTheCamera
+//
+//  Created by Дмитрий Рузайкин on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
